08b5d41a49fbc508551db54ea370a38b7b406c67
Raghu K <raghuk.vit@gmail.com>
2022-08-09 07:08:24 +0530

    Update terraform/immutable-app-ami/

--- ec2.tf
+++ ec2.tf
@@ -7,17 +7,33 @@ resource "aws_instance" "instance" {
   }
 }
 
+resource "null_resource" "copy-local-artifact" {
+  provisioner "file" {
+
+    connection {
+      user     = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_USER"]
+      password = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_PASS"]
+      host     = aws_instance.instance.private_ip
+    }
+
+    source      = "${var.COMPONENT}-${var.APP_VERSION}.zip"
+    destination = "/tmp/${var.COMPONENT}.zip"
+
+  }
+}
+
 resource "null_resource" "ansible" {
+  depends_on = [null_resource.copy-local-artifact]
   provisioner "remote-exec" {
 
     connection {
       user     = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_USER"]
       password = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_PASS"]
       host     = aws_instance.instance.private_ip
-
-      inline = [
-        "ansible-pull -U https://github.com/raghudevopsb65/roboshop-ansible.git roboshop.yml -e HOST=localhost -e ROLE=${var.COMPONENT} -e ENV=ENV -e DOCDB_ENDPOINT=DOCDB_ENDPOINT -e REDIS_ENDPOINT=REDIS_ENDPOINT -e MYSQL_ENDPOINT=MYSQL_ENDPOINT",
-      ]
     }
+
+    inline = [
+      "ansible-pull -U https://github.com/raghudevopsb65/roboshop-ansible.git roboshop.yml -e HOST=localhost -e ROLE=${var.COMPONENT} -e ENV=ENV -e DOCDB_ENDPOINT=DOCDB_ENDPOINT -e REDIS_ENDPOINT=REDIS_ENDPOINT -e MYSQL_ENDPOINT=MYSQL_ENDPOINT",
+    ]
   }
 }
