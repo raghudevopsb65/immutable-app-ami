f4d01ec2994b1e47befd82337c190ae57d282e33
Raghu K <raghuk.vit@gmail.com>
2022-08-10 06:21:42 +0530

    Update terraform/immutable-app-ami/

--- ami.tf
+++ ami.tf
@@ -1,4 +1,5 @@
 resource "aws_ami_from_instance" "ami" {
+  depends_on         = [null_resource.ansible]
   name               = "${var.COMPONENT}-${var.APP_VERSION}"
   source_instance_id = aws_instance.instance.id
 }
